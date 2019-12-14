711845367da6af6ab246adddf3c37a5221560a43
SSANCA02 <ssanca02@estudiantes.unileon.es>
2019-12-14 23:32:33 +0100

    Sanitizacion y correccion de otros errores

--- src/com/javatpoint/LoginServlet.java
+++ src/com/javatpoint/LoginServlet.java
@@ -1,9 +1,11 @@
 package com.javatpoint;
 
 import java.io.IOException;
+
 import java.io.PrintWriter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
@@ -13,22 +15,58 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		response.setContentType("text/html");
-		PrintWriter out=response.getWriter();
-		
-		if(!request.getParameter("content").matches("[\\w*\\s*]*")){
-			request.setAttribute("error", "Please enter only letters, numbers, and spaces.");
-			request.getRequestDispatcher("WEB-INF/jsp/render.jsp").forward(request,response);
-			return;
-		}
-		request.getRequestDispatcher("link.html").include(request, response);
-		
+
 		/*
          * No existe un metodo para comprobar las entradas del usuario
          * 
          * Hay que sanitizar las entradas y limpiar
          * La mejor manera de sanitizar entradas para prevenir XSS
          * en una aplicacion web es usar whitelist, para que solo pueda entrar letras numeros y espacios
+         * 
+         */
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		/*
+		 * Las siguientes lienas de codigo saltan un error si el usuario pone caracteres
+		 * extranios 
+		 * 
+		 */
+		
+		if(!request.getParameter("name").matches("[\\w*\\s*]*")){
+			request.setAttribute("error", "Please enter only letters, numbers, and spaces.");
+			request.getRequestDispatcher("WEB-INF/jsp/render.jsp").forward(request,response);
+			return;
+		}
+		
+		if(!request.getParameter("password").matches("[\\w*\\s*]*")){
+			request.setAttribute("error", "Please enter only letters, numbers, and spaces.");
+			request.getRequestDispatcher("WEB-INF/jsp/render.jsp").forward(request,response);
+			return;
+		}
+		
+		/*
+		 * Tambien podriamos usar jsoup
+		 * 
+		 * La biblioteca jSoup proporciona funcionalidad para limpiar HTML y permite solo
+		 * ciertas etiquetas en el contenido. 
+		 * Para usar jSoup, primero hay que pensar en las etiquetas que quieres permitir
+		 * y luego busca una lista blanca que coincida con lo que quieres. 
+		 * Luego simplemente pase la lista blanca a la función Jsoup.clean() 
+		 * junto con el contenido que queremos limpiar
+		 * 
+		 * String name = Jsoup.clean(request.getParameter("name"), Whitelist.basic());
+		 * char[]  password = Jsoup.clean(request.getParameter("password"), Whitelist.basic());
+		 */
+		
+		
+		request.getRequestDispatcher("link.html").include(request, response);
+		
+        /*
+         * Tambien podriamos controlar el tamanio del name y del password,por ejemplo, que fuera
+         * menor de 20. Simplemente seria un if que controlara que ambos tuvieran menos de 20 y si su
+         * length es menor de 20 se imprimiria un error por pantalla 
          * 
          * A parte, se ha leido el nombre y la contrasenia y se han almacenado en un string
          * el nombre y la contrasenia estaran expuestos hasta que el recolector de basura pase
@@ -59,7 +97,9 @@ public class LoginServlet extends HttpServlet {
 		
 		password = cifrarContrasenia(password);
 		
-		if(password.equals("admin123")){
+
+		if(Arrays.equals(password, passQueNoDeberiaEstarEnclaro)){
+			
 			out.print("You are successfully logged in!");
 			out.print("<br>Welcome, "+name);
 			
@@ -82,9 +122,8 @@ public class LoginServlet extends HttpServlet {
 			
 			if(request.getCookies()[0]!=null && request.getCookies()[0].getValue()!=null) {
 				String[] value = request.getCookies()[0].getValue().split(";");
-				if (value.length !=2) {
-					out.print("Username or pass error!");
-				}
+				Cookie ck=new Cookie("name",name);
+				response.addCookie(ck);
 			} else {
 				validated = isUserValid(name, password);
 				if(!validated) {
@@ -117,7 +156,6 @@ public class LoginServlet extends HttpServlet {
 	 */
 
 	private char[] cifrarContrasenia(char[] pass) {
-
 		MessageDigest md = null;
 		try {
 			md = MessageDigest.getInstance("SHA-256");
@@ -135,7 +173,6 @@ public class LoginServlet extends HttpServlet {
 		for(byte b : hash) {        
 			sb.append(String.format("%02x", b));
 		}
-		    
 		return sb.toString().toCharArray();
 		
 	}
